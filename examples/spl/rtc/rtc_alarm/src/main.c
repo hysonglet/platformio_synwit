ba092a011f5eb97f18afdc76bda4757631346844
hysonglet <hysonglet@qq.com>
2023-03-25 20:58:21 +0800

    1. 增加闹钟测试例程 2. 存在闹钟中断多次进入，中断标志清除不及时吧 ----- By Hysonglet

--- examples/spl/rtc/rtc_alarm/src/main.c
+++ examples/spl/rtc/rtc_alarm/src/main.c
@@ -6,27 +6,25 @@
 int main(void)
 {
     uart0_init();
-    rtc_init(2023, 3, 25, 19, 45, 0, false, true);
-
-    for (;;)
-    {
-        delay_ms(1000);
-        RTC_DateTime dt;
-        rtc_datetime_get(&dt);
-        printf("%u/%u/%u %u:%u:%u\n", dt.Year, dt.Month, dt.Date,
-                                        dt.Hour, dt.Minute, dt.Second);
+    rtc_init(2023, 3, 25, 19, 45, 0, true, true);
+    rtc_alarm_init(RTC_SUN|RTC_MON|RTC_THU|RTC_WED|RTC_TUE|RTC_FRI|RTC_SAT,
+            19, 45, 30, true);
+    for (;;){
     }
     return 0;
 }
 
 void RTC_Handler(void)
 {
-	if(RTC_IntSecondStat(RTC))
-	{
+    if(RTC_IntAlarmStat(RTC)){
+		RTC_IntAlarmClr(RTC);
+        printf("alarm comming...\n");
+    }
+	if(RTC_IntSecondStat(RTC)){
 		RTC_IntSecondClr(RTC);
         RTC_DateTime dt;
         rtc_datetime_get(&dt);
-        printf("second int %u/%u/%u %u:%u:%u\n", dt.Year, dt.Month, dt.Day,
+        printf("second int %u/%u/%u %u:%u:%u\n", dt.Year, dt.Month, dt.Date,
                                         dt.Hour, dt.Minute, dt.Second);
 	}
     if(RTC_IntMinuteStat(RTC)){
